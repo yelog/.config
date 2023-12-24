b148f3f6e57405271123eed526cbcc935b34e1cf
yelog <yangyj13@lenovo.com>
2023-12-24 14:37:49 +0800

    change: ban search mode with flash.nvim

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -1,34 +1,49 @@
 -- 通过搜索字符的方式进行跳转
 return {
-	"folke/flash.nvim",
-	event = "VeryLazy",
-	---@typ Flash.Config
-	opts = {},
-	keys = {
-		{
-			"s",
-			mode = { "n", "x", "o" },
-			function()
-				-- default options: exact mode, multi window, all directions, with a backdrop
-				require("flash").jump()
-			end,
-			desc = "Flash",
-		},
-		{
-			"S",
-			mode = { "n", "o", "x" },
-			function()
-				require("flash").treesitter()
-			end,
-			desc = "Flash Treesitter",
-		},
-		{
-			"r",
-			mode = "o",
-			function()
-				require("flash").remote()
-			end,
-			desc = "Remote Flash",
-		},
-	},
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  ---@typ Flash.Config
+  opts = {
+    modes = {
+      search = {
+        -- when `true`, flash will be activated during regular search by default.
+        -- You can always toggle when searching with `require("flash").toggle()`
+        enabled = false,
+        highlight = { backdrop = false },
+        jump = { history = true, register = true, nohlsearch = true },
+        search = {
+          -- `forward` will be automatically set to the search direction
+          -- `mode` is always set to `search`
+          -- `incremental` is set to `true` when `incsearch` is enabled
+        },
+      }
+    },
+  },
+  keys = {
+    {
+      "s",
+      mode = { "n", "x", "o" },
+      function()
+        -- default options: exact mode, multi window, all directions, with a backdrop
+        require("flash").jump()
+      end,
+      desc = "Flash",
+    },
+    {
+      "S",
+      mode = { "n", "o", "x" },
+      function()
+        require("flash").treesitter()
+      end,
+      desc = "Flash Treesitter",
+    },
+    {
+      "r",
+      mode = "o",
+      function()
+        require("flash").remote()
+      end,
+      desc = "Remote Flash",
+    },
+  },
 }
