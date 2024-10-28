229a219b0f28dbbedfe2fc5ec045bc5967be2269
yelog <yelogeek@gmail.com>
2024-10-28 10:55:45 +0800

    update

--- nvim/lua/plugins/spectre.lua
+++ nvim/lua/plugins/spectre.lua
@@ -3,6 +3,18 @@ return {
   "nvim-pack/nvim-spectre",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
+    require("spectre").setup({
+      replace_engine = {
+        ["sed"] = {
+          cmd = "sed",
+          args = {
+            "-i",
+            "",
+            "-E",
+          },
+        },
+      },
+    })
     vim.keymap.set('n', '<M-S-R>', '<cmd>lua require("spectre").toggle()<CR>', {
       desc = "Toggle Spectre"
     })
