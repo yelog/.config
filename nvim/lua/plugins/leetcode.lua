6142deda17cefe5acf036aab96560ba0589e2153
yelog <yelogeek@gmail.com>
2024-11-11 09:49:14 +0800

    update

--- nvim/lua/plugins/leetcode.lua
+++ nvim/lua/plugins/leetcode.lua
@@ -1,5 +1,5 @@
 return {
- "kawre/leetcode.nvim",
+  "kawre/leetcode.nvim",
   build = ":TSUpdate html",
   dependencies = {
     "nvim-telescope/telescope.nvim",
@@ -12,78 +12,92 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   opts = {
-    {
-      ---@type string
-      arg = "leetcode.nvim",
+    ---@type string
+    arg = "leetcode.nvim",
 
-      ---@type lc.lang
-      lang = "cpp",
+    ---@type lc.lang
+    lang = "typescript",
 
-      cn = { -- leetcode.cn
-        enabled = false, ---@type boolean
-        translator = true, ---@type boolean
-        translate_problems = true, ---@type boolean
+    cn = { -- leetcode.cn
+      enabled = true, ---@type boolean
+      translator = true, ---@type boolean
+      translate_problems = true, ---@type boolean
+    },
+
+    ---@type lc.storage
+    storage = {
+      home = vim.fn.stdpath("data") .. "/leetcode",
+      cache = vim.fn.stdpath("cache") .. "/leetcode",
+    },
+
+    ---@type table<string, boolean>
+    plugins = {
+      non_standalone = false,
+    },
+
+    ---@type boolean
+    logging = true,
+
+    injector = {}, ---@type table<lc.lang, lc.inject>
+
+    cache = {
+      update_interval = 60 * 60 * 24 * 7, ---@type integer 7 days
+    },
+
+    console = {
+      open_on_runcode = true, ---@type boolean
+
+      dir = "row", ---@type lc.direction
+
+      size = { ---@type lc.size
+        width = "90%",
+        height = "75%",
       },
 
-      ---@type string
-      directory = vim.fn.stdpath("data") .. "/leetcode/",
-
-      ---@type boolean
-      logging = true,
-
-      cache = {
-        update_interval = 60 * 60 * 24 * 7, ---@type integer 7 days
+      result = {
+        size = "60%", ---@type lc.size
       },
 
-      console = {
-        open_on_runcode = true, ---@type boolean
+      testcase = {
+        virt_text = true, ---@type boolean
 
-        dir = "row", ---@type lc.direction
-
-        size = { ---@type lc.size
-          width = "90%",
-          height = "75%",
-        },
-
-        result = {
-          size = "60%", ---@type lc.size
-        },
-
-        testcase = {
-          virt_text = true, ---@type boolean
-
-          size = "40%", ---@type lc.size
-        },
+        size = "40%", ---@type lc.size
       },
+    },
 
-      description = {
-        position = "left", ---@type lc.position
+    description = {
+      position = "left", ---@type lc.position
 
-        width = "40%", ---@type lc.size
+      width = "40%", ---@type lc.size
 
-        show_stats = true, ---@type boolean
-      },
+      show_stats = true, ---@type boolean
+    },
 
-      hooks = {
-        ---@type fun()[]
-        LeetEnter = {},
+    hooks = {
+      ---@type fun()[]
+      ["enter"] = {},
 
-        ---@type fun(question: { lang: string })[]
-        LeetQuestionNew = {},
-      },
+      ---@type fun(question: lc.ui.Question)[]
+      ["question_enter"] = {},
 
-      ---@type boolean
-      image_support = false, -- setting this to `true` will disable question description wrap
+      ---@type fun()[]
+      ["leave"] = {},
+    },
 
-      keys = {
-        toggle = { "q", "<Esc>" }, ---@type string|string[]
-        confirm = { "<CR>" }, ---@type string|string[]
+    keys = {
+      toggle = { "q" }, ---@type string|string[]
+      confirm = { "<CR>" }, ---@type string|string[]
 
-        reset_testcases = "r", ---@type string
-        use_testcase = "U", ---@type string
-        focus_testcases = "H", ---@type string
-        focus_result = "L", ---@type string
-      },
-    }
+      reset_testcases = "r", ---@type string
+      use_testcase = "U", ---@type string
+      focus_testcases = "H", ---@type string
+      focus_result = "L", ---@type string
+    },
+
+    ---@type lc.highlights
+    theme = {},
+
+    ---@type boolean
+    image_support = true,
   },
 }
--- nvim/lua/plugins/style/indent-blankline.lua
+++ nvim/lua/plugins/style/indent-blankline.lua
@@ -1,16 +1,16 @@
 -- 显示同一个代码块, 通过在前面显示竖条
 return {
-  "lukas-reineke/indent-blankline.nvim", --> Indent guides for Neovim
-  main = "ibl",
-  config = function()
-    vim.opt.list = true
-    -- vim.opt.listchars:append "eol:↴"
-
-    require("ibl").setup({
-      exclude = {
-        -- filetypes = { "help", "dashboard", "packer", "neogitstatus", "NvimTree", "vista", "sagahover", "sagasignature", "sagaaprdoc", "sagaaprsignature" }
-        filetypes = { "dashboard" }
-      }
-    })
-  end,
+  -- "lukas-reineke/indent-blankline.nvim", --> Indent guides for Neovim
+  -- main = "ibl",
+  -- config = function()
+  --   vim.opt.list = true
+  --   -- vim.opt.listchars:append "eol:↴"
+  --
+  --   require("ibl").setup({
+  --     exclude = {
+  --       -- filetypes = { "help", "dashboard", "packer", "neogitstatus", "NvimTree", "vista", "sagahover", "sagasignature", "sagaaprdoc", "sagaaprsignature" }
+  --       filetypes = { "dashboard" }
+  --     }
+  --   })
+  -- end,
 }
