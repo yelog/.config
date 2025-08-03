9933c6115a0a6710c508c247fa62e80ee36eb898
yelog <yelogeek@gmail.com>
2025-08-03 17:44:20 +0800

    update

--- nvim/lua/plugins/lsp/cmp.lua
+++ nvim/lua/plugins/lsp/cmp.lua
@@ -1,194 +1,4 @@
 return {
-  -- {
-  --   "L3MON4D3/LuaSnip",
-  --   dependencies = { "rafamadriz/friendly-snippets" },
-  --   -- install jsregexp (optional!).
-  --   build = "make install_jsregexp",
-  --   config = function()
-  --     -- require("luasnip").filetype_extend("ruby", { "rails" })
-  --     require("luasnip.loaders.from_vscode").lazy_load {
-  --       exclude = { "markdown" },
-  --     }
-  --     require("luasnip.loaders.from_snipmate").lazy_load()
-  --   end,
-  -- },
-  -- {
-  --   "hrsh7th/nvim-cmp",
-  --   dependencies = {
-  --     "luckasRanarison/tailwind-tools.nvim",
-  --     "onsails/lspkind-nvim",
-  --   },
-  --   config = function()
-  --     local cmp = require("cmp")
-  --     local luasnip = require("luasnip")
-  --     local compare = require("cmp.config.compare")
-  --     local lspkind = require('lspkind')
-  --
-  --     cmp.setup({
-  --       snippet = {
-  --         -- REQUIRED - you must specify a snippet engine
-  --         expand = function(args)
-  --           -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-  --           luasnip.lsp_expand(args.body) -- For `luasnip` users.
-  --           -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-  --           -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-  --         end,
-  --       },
-  --       window = {
-  --         documentation = false,
-  --         -- documentation = {
-  --         --   border = "rounded",
-  --         --   winhighlight = "NormalFloat:Pmenu,NormalFloat:Pmenu,CursorLine:PmenuSel,Search:None",
-  --         -- },
-  --         completion = {
-  --           border = "rounded",
-  --           winhighlight = "NormalFloat:Pmenu,NormalFloat:Pmenu,CursorLine:PmenuSel,Search:None",
-  --         },
-  --       },
-  --       experimental = {
-  --         ghost_text = true,
-  --       },
-  --       mapping = cmp.mapping.preset.insert({
-  --         -- ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-  --         -- ['<C-f>'] = cmp.mapping.scroll_docs(4),
-  --         -- ['<Tab>'] = cmp.mapping.complete(),
-  --         -- ['<C-e>'] = cmp.mapping.abort(),
-  --         ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-  --         ["<Tab>"] = cmp.mapping(function(fallback)
-  --           if cmp.visible() then
-  --             cmp.select_next_item()
-  --           elseif luasnip.jumpable(1) then
-  --             luasnip.jump(1)
-  --           elseif luasnip.expand_or_jumpable() then
-  --             luasnip.expand_or_jump()
-  --           elseif luasnip.expandable() then
-  --             luasnip.expand()
-  --           else
-  --             fallback()
-  --           end
-  --         end, {
-  --           "i",
-  --           "s",
-  --         }),
-  --         ["<S-Tab>"] = cmp.mapping(function(fallback)
-  --           if cmp.visible() then
-  --             cmp.select_prev_item()
-  --           elseif luasnip.jumpable(-1) then
-  --             luasnip.jump(-1)
-  --           else
-  --             fallback()
-  --           end
-  --         end, {
-  --           "i",
-  --           "s",
-  --         }),
-  --       }),
-  --       sources = cmp.config.sources({
-  --         {
-  --           name = "nvim_lsp",
-  --           filter = function(entry, ctx)
-  --             local kind = require("cmp.types.lsp").CompletionItemKind[entry:get_kind()]
-  --             if kind == "Snippet" then
-  --               return true
-  --             end
-  --
-  --             if kind == "Text" then
-  --               return true
-  --             end
-  --           end,
-  --           group_index = 2,
-  --         },
-  --         { name = "luasnip", group_index = 2 }, -- For luasnip users
-  --         {
-  --           name = "buffer",
-  --           group_index = 2,
-  --         }, -- For buffer words
-  --         -- { name = "tmux", group_index = 2 }, -- For tmux words
-  --         -- { name = 'vsnip' }, -- For vsnip users
-  --         { name = "path" }, -- For filesystem paths
-  --         -- { name = "rg" }, -- For filesystem paths
-  --         -- { name = 'ultisnips' }, -- For ultisnips users
-  --         -- { name = 'snippy' }, -- For snippy users
-  --         -- { name = "marksman" },
-  --         -- { name = "tsserver" },
-  --         -- { name = "bash-language-server" },
-  --         -- { name = "obsidian" },
-  --         { name = "ottor" },
-  --       }),
-  --       sorting = {
-  --         priority_weight = 2,
-  --         comparators = {
-  --           -- require("copilot_cmp.comparators").prioritize,
-  --           -- require("copilot_cmp.comparators").score,
-  --           compare.offset,
-  --           compare.exact,
-  --           -- compare.scopes,
-  --           compare.score,
-  --           compare.recently_used,
-  --           compare.locality,
-  --           -- compare.kind,
-  --           compare.sort_text,
-  --           compare.length,
-  --           compare.order,
-  --           -- require("copilot_cmp.comparators").prioritize,
-  --           -- require("copilot_cmp.comparators").score,
-  --         },
-  --       },
-  --       formatting = {
-  --         format = lspkind.cmp_format({
-  --           mode = 'symbol', -- show only symbol annotations
-  --           maxwidth = 50,   -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-  --           -- can also be a function to dynamically calculate max width such as
-  --           -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
-  --           ellipsis_char = '...',    -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-  --           show_labelDetails = true, -- show labelDetails in menu. Disabled by default
-  --
-  --           -- The function below will be called before any actual modifications from lspkind
-  --           -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-  --           -- before = function(entry, vim_item)
-  --           --   return vim_item
-  --           -- end
-  --           before = require("tailwind-tools.cmp").lspkind_format
-  --         })
-  --       },
-  --     })
-  --
-  --     -- Set configuration for specific filetype.
-  --     cmp.setup.filetype("gitcommit", {
-  --       sources = cmp.config.sources({
-  --         { name = "cmp_git" }, -- You can specify the `cmp_git` source if you were installed it.
-  --       }, {
-  --         { name = "buffer" },
-  --       }),
-  --     })
-  --
-  --     -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-  --     cmp.setup.cmdline({ "/", "?" }, {
-  --       mapping = cmp.mapping.preset.cmdline(),
-  --       sources = {
-  --         { name = "buffer" },
-  --       },
-  --     })
-  --
-  --     -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-  --     cmp.setup.cmdline(":", {
-  --       mapping = cmp.mapping.preset.cmdline(),
-  --       sources = cmp.config.sources({
-  --         { name = "path" },
-  --       }, {
-  --         { name = "cmdline" },
-  --       }),
-  --     })
-  --   end,
-  -- },
-  -- 'saadparwaiz1/cmp_luasnip',
-  -- "hrsh7th/cmp-buffer",     -- nvim-cmp source for buffer words
-  -- "hrsh7th/cmp-nvim-lsp",
-  -- "andersevenrud/cmp-tmux", -- tmux completion source for nvim-cmp
-  -- "hrsh7th/cmp-path",       -- nvim-cmp source for filesystem paths
-  -- "hrsh7th/cmp-cmdline",    -- use to command/search complete
-  -- "octaltree/cmp-look",
-  -- "lukas-reineke/cmp-rg",   -- ripgrep source for nvim-cmp
   {
     'saghen/blink.cmp',
     lazy = false, -- lazy loading handled internally
@@ -218,42 +28,29 @@ return {
       -- your own keymap.
       keymap = {
         preset = 'default',
-        ["<S-Tab>"] = { "select_prev", "fallback" },
-        ["<Tab>"] = { "select_next", "fallback" },
-        ["<UP>"] = { "select_prev", "fallback" },
-        ["<DOWN>"] = { "select_next", "fallback" },
-        ['<CR>'] = {
-          function(cmp)
-            -- local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-            if cmp.snippet_active() then
-              return cmp.accept()
-            else
-              return cmp.select_and_accept()
-            end
-          end,
-          'snippet_forward',
-          'fallback'
-        },
+        ["<Tab>"] = { "select_and_accept", "fallback" },
       },
       cmdline = {
+        completion = {
+          menu = {
+            auto_show = true,
+          },
+        },
         keymap = {
-          preset = 'enter',
-          ["<S-Tab>"] = { "select_prev", "fallback" },
-          ["<Tab>"] = { "select_next", "fallback" },
-          ["<UP>"] = { "select_prev", "fallback" },
-          ["<DOWN>"] = { "select_next", "fallback" },
+          preset = 'default',
+          ["<Tab>"] = { "select_and_accept", "fallback" },
         }
       },
 
-      appearance = {
-        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-        -- Useful for when your theme doesn't support blink.cmp
-        -- will be removed in a future release
-        use_nvim_cmp_as_default = true,
-        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-        -- Adjusts spacing to ensure icons are aligned
-        nerd_font_variant = 'mono'
-      },
+      -- appearance = {
+      --   -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+      --   -- Useful for when your theme doesn't support blink.cmp
+      --   -- will be removed in a future release
+      --   use_nvim_cmp_as_default = true,
+      --   -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+      --   -- Adjusts spacing to ensure icons are aligned
+      --   nerd_font_variant = 'mono'
+      -- },
 
       completion = {
         -- 'prefix' will fuzzy match on the text before the cursor
@@ -271,23 +68,43 @@ return {
         -- list = { selection = function(ctx) return ctx.mode == 'cmdline' and 'auto_insert' or 'preselect' end },
 
         -- Show documentation when selecting a completion item
-        documentation = { auto_show = true, auto_show_delay_ms = 500 },
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 500,
+          window = { border = 'single' },
+        },
 
         -- Display a preview of the selected item on the current line
         ghost_text = { enabled = true },
+
+        menu = {
+          auto_show = true,   -- automatically show the menu when typing
+          border = 'rounded', -- border style for the menu
+          draw = {
+            -- We don't need label_description now because label and label_description are already
+            -- combined together in label by colorful-menu.nvim.
+            columns = { { "kind_icon" }, { "label", gap = 1 } },
+            components = {
+              label = {
+                text = function(ctx)
+                  return require("colorful-menu").blink_components_text(ctx)
+                end,
+                highlight = function(ctx)
+                  return require("colorful-menu").blink_components_highlight(ctx)
+                end,
+              },
+            },
+          },
+        },
       },
-
-
       -- default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, via `opts_extend`
       sources = {
         default = { 'avante', 'lsp', 'path', 'snippets', 'buffer' },
         -- optionally disable cmdline completions
         -- cmdline = {},
-        per_filetype = {
-          codecompanion = { "codecompanion" },
-        },
         providers = {
+          lsp = { fallbacks = {} },
           avante = {
             module = 'blink-cmp-avante',
             name = 'Avante',
@@ -298,11 +115,6 @@ return {
           -- codeium = { name = 'Codeium', module = 'codeium.blink', async = true }
         }
       },
-      -- experimental signature help support
-      -- signature = { enabled = true }
     },
-    -- allows extending the providers array elsewhere in your config
-    -- without having to redefine it
-    opts_extend = { "sources.default" }
   },
 }
