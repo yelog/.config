f1ed4ca6af80f2e0c3858ccc137e56e2846553cd
yelog <yelogeek@gmail.com>
2025-09-05 18:01:42 +0800

    feat(i18n): add java ft and extend locale/file/func patterns
    - Add java to ft
    
    - Rename langs -> locales and use zh,en
    
    - Update file patterns to use {locales} and add properties examples
    
    - Add func_pattern entries for t,  and LangUtil.get
    
    - Rename keymap command I18nNextLang -> I18nNextLocale

--- nvim/lua/plugins/lsp/i18n.lua
+++ nvim/lua/plugins/lsp/i18n.lua
@@ -1,7 +1,7 @@
 return {
   'yelog/i18n.nvim',
   lazy = true,
-  ft = { "vue", "typescript" },
+  ft = { "vue", "typescript", "java" },
   dependencies = {
     'ibhagwan/fzf-lua',
     'nvim-treesitter/nvim-treesitter'
@@ -9,18 +9,27 @@ return {
   dev = true,
   config = function()
     require('i18n').setup({
-      -- langs = { "zh_CN", "en_US" },
-      langs = { 'zh', 'en' },
+      -- locales = { "en_US", "zh_CN" },
+      locales = { "zh", "en" },
       files = {
-        'src/locales/{langs}.json',
-        -- { files = "src/locales/lang/{langs}/{module}.ts",            prefix = "{module}." },
-        -- { files = "src/views/{bu}/locales/lang/{langs}/{module}.ts", prefix = "{bu}.{module}." },
-        -- { files = "packages/locales/src/langs/{langs}/{module}.json", prefix = "{module}." },
-      }
+        'src/locales/{locales}.json',
+        -- { files = 'src/locales/lang/{locales}/{module}.ts', prefix = "{module}." },
+        -- 'moss-service-mes-kitting/moss-service-mes-kitting-server/src/main/resources/static/i18n/messages_{locales}.properties',
+        -- 'moss-auth/src/main/resources/static/i18n/messages_{locales}.properties',
+        -- { files = "src/views/{bu}/locales/lang/{locales}/{module}.ts", prefix = "{bu}.{module}." },
+        -- { files = "packages/locales/src/locales/{locales}/{module}.json", prefix = "{module}." },
+      },
+      func_pattern = {
+        "t%(['\"]([^'\"]+)['\"]",
+        "%$t%(['\"]([^'\"]+)['\"]",
+        "LangUtil.get%([\"]([^\"]+)[\"]",
+      },
     })
 
-    vim.keymap.set("n", "<leader>fi", require("i18n.integration.fzf").show_i18n_keys_with_fzf, { desc = "Fuzzy search i18n key" })
-    vim.keymap.set("n", "<D-S-n>", require("i18n.integration.fzf").show_i18n_keys_with_fzf, { desc = "Fuzzy search i18n key" })
-    vim.keymap.set("n", "<D-S-M-n>", "<cmd>I18nNextLang<cr>", { desc = "Switch default I18n language" })
+    vim.keymap.set("n", "<leader>fi", require("i18n.integration.fzf").show_i18n_keys_with_fzf,
+      { desc = "Fuzzy search i18n key" })
+    vim.keymap.set("n", "<D-S-n>", require("i18n.integration.fzf").show_i18n_keys_with_fzf,
+      { desc = "Fuzzy search i18n key" })
+    vim.keymap.set("n", "<D-S-M-n>", "<cmd>I18nNextLocale<cr>", { desc = "Switch default I18n language" })
   end
 }
