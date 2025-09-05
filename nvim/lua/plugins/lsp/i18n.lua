191575cbf2a62503945650959410737cc095d9e9
yelog <yelogeek@gmail.com>
2025-09-05 09:49:23 +0800

    chore: update i18n.nvim plugin config
    - Enable lazy loading and set ft to vue, typescript
    
    - Add nvim-treesitter dependency
    
    - Simplify i18n setup to langs {zh, en} and use src/locales/{langs}.json
    
    - Translate keymap descriptions to English

--- nvim/lua/plugins/lsp/i18n.lua
+++ nvim/lua/plugins/lsp/i18n.lua
@@ -1,28 +1,26 @@
 return {
-  "yelog/i18n.nvim",
-  lazy = false, -- lazy loading handled internally
+  'yelog/i18n.nvim',
+  lazy = true,
+  ft = { "vue", "typescript" },
   dependencies = {
-    "ibhagwan/fzf-lua"
+    'ibhagwan/fzf-lua',
+    'nvim-treesitter/nvim-treesitter'
   },
   dev = true,
   config = function()
-    require("i18n").setup({
-      mode = 'static',
-      static = {
-        langs = { "zh_CN", "en_US" },
-        -- langs = { "zh", "en" },
-        files = {
-          -- "src/locales/{langs}.json",
-          { files = "src/locales/lang/{langs}/{module}.ts",            prefix = "{module}." },
-          { files = "src/views/{bu}/locales/lang/{langs}/{module}.ts", prefix = "{bu}.{module}." },
-          -- { files = "packages/locales/src/langs/{langs}/{module}.json", prefix = "{module}." },
-          -- { files = "src/views/{module}/lang/{langs}.json", prefix = "{module}." }
-        }
+    require('i18n').setup({
+      -- langs = { "zh_CN", "en_US" },
+      langs = { 'zh', 'en' },
+      files = {
+        'src/locales/{langs}.json',
+        -- { files = "src/locales/lang/{langs}/{module}.ts",            prefix = "{module}." },
+        -- { files = "src/views/{bu}/locales/lang/{langs}/{module}.ts", prefix = "{bu}.{module}." },
+        -- { files = "packages/locales/src/langs/{langs}/{module}.json", prefix = "{module}." },
       }
     })
 
-    vim.keymap.set("n", "<leader>fi", require("i18n.integration.fzf").show_i18n_keys_with_fzf, { desc = "模糊查找 i18n key" })
-    vim.keymap.set("n", "<D-S-n>", require("i18n.integration.fzf").show_i18n_keys_with_fzf, { desc = "模糊查找 i18n key" })
-    vim.keymap.set("n", "<D-S-M-n>", "<cmd>I18nNextLang<cr>", { desc = "切换 I18n 默认语言" })
+    vim.keymap.set("n", "<leader>fi", require("i18n.integration.fzf").show_i18n_keys_with_fzf, { desc = "Fuzzy search i18n key" })
+    vim.keymap.set("n", "<D-S-n>", require("i18n.integration.fzf").show_i18n_keys_with_fzf, { desc = "Fuzzy search i18n key" })
+    vim.keymap.set("n", "<D-S-M-n>", "<cmd>I18nNextLang<cr>", { desc = "Switch default I18n language" })
   end
 }
