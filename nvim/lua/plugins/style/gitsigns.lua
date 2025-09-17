b20e7d64b013d522a94e6e71330eade5708b8579
yelog <yelogeek@gmail.com>
2025-09-17 23:46:35 +0800

    fix(gitsigns): use :Gitsigns blame for <leader>gb and remove <D-W> mapping
    - Comment out gs.blame_line mapping
    
    - Add <leader>gb mapping that calls vim.cmd('Gitsigns blame')
    
    - Remove <D-W> diffthis('~') mapping

--- nvim/lua/plugins/style/gitsigns.lua
+++ nvim/lua/plugins/style/gitsigns.lua
@@ -81,12 +81,14 @@ return {
         map('n', '<leader>gu', gs.undo_stage_hunk, { desc = 'Git Undo Stage Hunk' })
         map('n', '<leader>gR', gs.reset_buffer, { desc = 'Git Reset Buffer' })
         map('n', '<leader>gp', gs.preview_hunk, { desc = 'Git Preview Hunk' })
-        map('n', '<leader>gb', function() gs.blame_line { full = true } end, { desc = 'Git Blame Line' })
+        -- map('n', '<leader>gb', function() gs.blame_line { full = true } end, { desc = 'Git Blame Line' })
         map('n', '<leader>tb', gs.toggle_current_line_blame, { desc = 'Git Toggle Current Line Blame' })
         map('n', '<leader>gd', gs.diffthis, { desc = 'Git Diff This' })
         map('n', '<leader>gD', function() gs.diffthis('~') end, { desc = 'Git Diff This ~' })
-        map('n', '<D-W>', function() gs.diffthis('~') end, { desc = 'Git Diff This ~' })
         map('n', '<leader>td', gs.toggle_deleted, { desc = 'Git Toggle Deleted' })
+        map('n', '<leader>gb', function()
+          vim.cmd('Gitsigns blame')
+        end, { desc = 'Git Toggle Deleted' })
 
         -- Text object
         map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = 'Git Select Hunk' })
