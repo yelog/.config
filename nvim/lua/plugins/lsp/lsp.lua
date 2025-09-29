6504988b86f8d0c960b6043bffb42cc17771a4d9
yelog <yelogeek@gmail.com>
2025-09-29 14:46:27 +0800

    refactor: make on_attach a local function
    - Define on_attach as a local function instead of assigning it directly in the table
    
    - Scope the LSP on_attach to the module rather than the table

--- nvim/lua/plugins/lsp/lsp.lua
+++ nvim/lua/plugins/lsp/lsp.lua
@@ -153,7 +153,7 @@ return {
 
       -- Use an on_attach function to only map the following keys
       -- after the language server attaches to the current buffer
-      on_attach = function(client, bufnr)
+      local on_attach = function(client, bufnr)
         -- Enable completion triggered by <c-x><c-o>
         -- vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
