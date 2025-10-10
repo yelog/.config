c8c7d8d3ab157dcfb3a5289bc301aa56e0a598be
yelog <yelogeek@gmail.com>
2025-10-10 09:26:47 +0800

    feat: add key mappings for i18n plugin
    - Add keys table with <D-S-n>, <D-S-B>, <D-S-J> handlers
    
    - Add i18n_keys popup_type config
    
    - Comment out previous vim.keymap.set mappings

--- nvim/lua/plugins/lsp/i18n.lua
+++ nvim/lua/plugins/lsp/i18n.lua
@@ -6,6 +6,11 @@ return {
     -- 'nvim-telescope/telescope.nvim',
     'nvim-treesitter/nvim-treesitter'
   },
+  keys = {
+    { "<D-S-n>", function() I18n.i18n_keys() end,   desc = "Show i18n keys" },
+    { "<D-S-B>", function() I18n.next_locale() end, desc = "Switch to next locale" },
+    { "<D-S-J>", function() I18n.toggle_origin() end, desc = "Toggle Origin" },
+  },
   dev = true,
   config = function()
     require('i18n').setup({
@@ -14,16 +19,16 @@ return {
         -- Available values: 'vim_ui', 'telescope', 'fzf-lua', 'snacks'
         popup_type = 'fzf-lua',
       },
+      i18n_keys = {
+        popup_type = 'fzf-lua'
+      }
     })
 
-    vim.keymap.set("n", "<leader>fi", require('i18n').show_i18n_keys_with_fzf,
-      { desc = "Fuzzy search i18n key" })
     -- vim.keymap.set("n", "<D-S-n>", require("i18n").show_i18n_keys_with_telescope,
     --   { desc = "Search i18n key (Telescope)" })
 
-    vim.keymap.set({ "n", 'i' }, "<D-S-n>", require('i18n').show_i18n_keys_with_fzf,
-      { desc = "Fuzzy search i18n key" })
-    vim.keymap.set("n", "<D-S-B>", "<cmd>I18nNextLocale<cr>", { desc = "Switch default I18n language" })
-    vim.keymap.set("n", "<D-S-J>", "<cmd>I18nToggleOrigin<cr>", { desc = "Switch default I18n language" })
+    -- vim.keymap.set({ "n", 'i' }, "<D-S-n>", require('i18n').i18n_keys, { desc = "Fuzzy search i18n key" })
+    -- vim.keymap.set("n", "<D-S-B>", "<cmd>I18nNextLocale<cr>", { desc = "Switch default I18n language" })
+    -- vim.keymap.set("n", "<D-S-J>", "<cmd>I18nToggleOrigin<cr>", { desc = "Switch default I18n language" })
   end
 }
