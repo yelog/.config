b5e4c5772201a7f92c24a4deb4abf3002f655c00
yelog <yelogeek@gmail.com>
2026-01-15 09:43:17 +0800

    update

--- hammerspoon/modules/key-map.lua
+++ hammerspoon/modules/key-map.lua
@@ -128,6 +128,7 @@ _G.applications = {
   { prefix = hyperKey, key = "U", message = "Teams",         bundleId = "com.microsoft.teams2" },
   { prefix = hyperKey, key = "M", message = "Mail",          bundleId = "com.apple.mail" },
   { prefix = hyperKey, key = ";", message = "ChatGPT",  bundleId = "com.openai.chat" },
+  -- { prefix = hyperKey, key = ";", message = "Cluade",  bundleId = "com.anthropic.claudefordesktop" },
   -- { prefix = hyperKey, key = ";", message = "Ollama",  bundleId = "com.electron.ollama" },
   -- { prefix = hyperKey, key = ";", message = "ChatGPT",       bundleId = "com.tencent.yuanbao" },
   -- { prefix = hyperKey, key = ";", message = "Browser",        bundleId = "com.google.Chrome" },
