de4f1ba2bf212b315413ddca951ce447c3078924
yelog <yelogeek@gmail.com>
2026-02-04 10:20:56 +0800

    update

--- nvim/lua/plugins/goto/flash.lua
+++ nvim/lua/plugins/goto/flash.lua
@@ -6,16 +6,7 @@ return {
   opts = {
     modes = {
       search = {
-        -- when `true`, flash will be activated during regular search by default.
-        -- You can always toggle when searching with `require("flash").toggle()`
         enabled = false,
-        highlight = { backdrop = false },
-        jump = { history = true, register = true, nohlsearch = true },
-        search = {
-          -- `forward` will be automatically set to the search direction
-          -- `mode` is always set to `search`
-          -- `incremental` is set to `true` when `incsearch` is enabled
-        },
       }
     },
   },
