73ca8064cf142f5279a597e0d191dbcb97a9448c
yelog <yelogeek@gmail.com>
2026-07-16 13:45:49 +0800

    feat(fzf-lua): show filenames before directories

--- nvim/lua/plugins/panel/fzf-lua.lua
+++ nvim/lua/plugins/panel/fzf-lua.lua
@@ -4,6 +4,9 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     require('fzf-lua').setup({
+      files = {
+        formatter = "path.filename_first",
+      },
       previewers = {
         builtin = {
           render_markdown = { enabled = false, filetypes = { ["markdown"] = true } }
