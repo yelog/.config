cf844b8b97a180247300891a2e807e7aa20ad67b
yelog <yelogeek@gmail.com>
2026-09-10 08:09:11 +0800

    update

--- nvim/lua/plugins/panel/lazydb.lua
+++ nvim/lua/plugins/panel/lazydb.lua
@@ -1,11 +1,14 @@
 return {
   "yelog/lazydb.nvim",
+  dev = true,
+  ft = { "sql", "xml" },
   cmd = {
     "LazyDB",
     "LazyDBToggle",
     "LazyDBHide",
     "LazyDBStop",
     "LazyDBRestart",
+    "LazyDBLspRestart",
   },
   keys = {
     {
@@ -17,7 +20,17 @@ return {
     },
   },
   opts = {
-    executable = "lazydb",
+    -- executable = "lazydb",
+    executable = "/Users/yelog/workspace/tui/lazydb/target/release/lazydb",
+    profile = "lssc-uat",
     window = { width = 0.92, height = 0.90, border = "rounded" },
+    lsp = {
+      enabled = true,
+      filetypes = { "sql", "xml" },
+      diagnostics = {
+        enabled = true,
+        debounce_ms = 300,
+      },
+    },
   },
 }
